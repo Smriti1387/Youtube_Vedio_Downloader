7707e2210413b88cd23ad8859c7bef4882b2d688
Smriti <54507520+Smriti1387@users.noreply.github.com>
2020-10-10 21:26:52 +0530

    Delete youtube.py

--- youtube.py
+++
@@ -1,8 +0,0 @@
-from pytube import YouTube 
-print("Provide the link of the youtube video here !! \n")
-yt = YouTube(input()) 
-
-print(yt.title) 
-stream = yt.streams.first() 
-stream.download()
-print("Downloaded!!")
