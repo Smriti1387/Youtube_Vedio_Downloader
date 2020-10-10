619a7eca38b1084bdd35bd6195788489171ad093
Smriti <54507520+Smriti1387@users.noreply.github.com>
2020-10-10 21:19:41 +0530

    Add files via upload

--- youtube.py
+++ youtube.py
@@ -2,9 +2,7 @@ from pytube import YouTube
 print("Provide the link of the youtube video here !! \n")
 yt = YouTube(input()) 
 
-try:
- print(yt.title) 
- stream = yt.streams.first() 
- stream.download()
-except:
- print("Connection Error!")
+print(yt.title) 
+stream = yt.streams.first() 
+stream.download()
+print("Downloaded!!")
